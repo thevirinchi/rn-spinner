301260b55130e5868aeac7f10610e62bc3f3c281
z3t4z00k <z3t4z00k@gmail.com>
2020-12-07 13:43:40 -0500

    Created a spinner component

---
+++ components/Spinner.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+
+const Spinner = props => {
+
+	return (
+		(
+			props.loadingState
+				?
+				// Overlay View
+				<View style={styles.overlay}>
+					{/* Spinner Box */}
+					<View style={styles.container}>
+						{/* React Native default Spinnner */}
+						<ActivityIndicator size="large" color="#ffd100" />
+						<Text>Loading...</Text>
+					</View>
+				</View>
+				:
+				<View />
+		)
+	)
+}
+
+const styles = StyleSheet.create({
+	overlay: { position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, height: "100%", width: "100%", justifyContent: "center", alignItems: "center" },
+	container: { padding: 24, borderRadius: 16, elevation: 4, shadowColor: "#212121", shadowOpacity: 0.25, shadowOffset: { width: 0, height: 2 }, shadowRadius: 8, justifyContent: "center", alignItems: "center", backgroundColor: "#fafafa" }
+})
+
+export default Spinner
